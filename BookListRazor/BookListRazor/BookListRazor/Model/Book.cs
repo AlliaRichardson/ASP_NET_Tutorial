e081d67bb0eec7bfa2473b24b8314e5218c02ac4
Allia <richardson.allia@gmail.comm>
2020-12-14 21:02:29 -0800

    Start
    section 3

---
+++ BookListRazor/BookListRazor/BookListRazor/Model/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required] //means name cannot be null
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
